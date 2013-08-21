b73c9071b8c52638d4037b1c83014852e4a9a164
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-21 06:57:25 -0400

    set up first real test

--- test/zipline_test.js
+++ test/zipline_test.js
@@ -1,15 +1,35 @@
+var express = require('express');
+var http = require('http');
 var supertest = require('supertest');
 
 var app = require('../src/app');
 
 describe('zipline', function(){
+  var createServer = function(){
+    var testApp = express();
+    testApp.get('/test.js', function(req, res){
+      res.sendfile(__filename);
+    });
+    return http.createServer(testApp);
+  };
+
+  var testServer = createServer();
+
+  before(function(done){
+    testServer.listen(8000, done);
+  });
+  after(function(){
+    testServer.close();
+  });
+
+
   var currentTime = function(){
     return Date.now() / 1000;
   };
 
-  it("should do stuff", function(done){
-    // supertest(app)
-    //   .get('/?time=123')
-    //   .expect(400, done);
+  it("should respond with an octet-stream", function(done){
+    supertest(app)
+      .get('/?file=http://localhost:8000/test.js')
+      .expect(200, done);
   });
 });
